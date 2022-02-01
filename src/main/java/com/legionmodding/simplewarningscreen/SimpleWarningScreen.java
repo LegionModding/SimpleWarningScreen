62becf815c032838fcec2485eb1772f4106662dc
LegionModding <legion@legionmodding.com>
2022-02-01 02:23:58 +0000

    Initial Commit, blank mod

---
+++ src/main/java/com/legionmodding/simplewarningscreen/SimpleWarningScreen.java
@@ -0,0 +1,36 @@
+package com.legionmodding.simplewarningscreen;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(SimpleWarningScreen.ID)
+public class SimpleWarningScreen
+{
+    public static final String ID = "simplewarningscreen";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public SimpleWarningScreen()
+    {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientSetup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onCommonSetup);;
+
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void onClientSetup(FMLClientSetupEvent event)
+    {
+
+    }
+
+    private void onCommonSetup(FMLCommonSetupEvent event)
+    {
+
+    }
+}
